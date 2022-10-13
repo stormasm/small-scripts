3fb05210f31ca3444e2bd762294b6cc00dd1a0ca
Marshall Bruner <brunerm99@gmail.com>
2022-10-13 15:36:56 -0600

    Now adds execute permissions prior to copying

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ files=$(ls $my_path)
 
 for file in $files; do
   prod=$(cat $file | awk '/production:/{print $NF}')
-  [ "$prod" = "true" ] && cp "$my_path/$file" "$HOME/.local/bin/"
+  [ "$prod" = "true" ] && chmod +x "$my_path/$file" && cp "$my_path/$file" "$HOME/.local/bin/"
 done
