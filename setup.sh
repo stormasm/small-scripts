8e0e2ec87d590f4ec421a31d5d31d35818cab334
Marshall Bruner <brunerm99@gmail.com>
2022-12-11 19:00:07 -0700

    Cleaned up setup

--- setup.sh
+++ setup.sh
@@ -5,5 +5,8 @@ files=$(ls $my_path)
 
 for file in $files; do
   prod=$(cat $file | awk '/production:/{print $NF}')
-  [ "$prod" = "true" ] && chmod +x "$my_path/$file" && cp "$my_path/$file" "$HOME/.local/bin/"
+  [ "$prod" = "true" ] && 
+      chmod +x "$my_path/$file" && 
+      cp "$my_path/$file" "$HOME/.local/bin/" &&
+      echo "Copied $file to $HOME/.local/bin/"
 done
