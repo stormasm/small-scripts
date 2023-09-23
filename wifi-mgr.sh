414a69e2c466e4b0ba24841acbbd7525097a6aa0
Marshall Bruner <brunerm99@gmail.com>
2023-09-23 18:12:52 +0200

    Cleanup

--- wifi-mgr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# wifi-mgr.sh
-
-notify-send "Getting Wi-Fi networks..."
-
-networks=$(nmcli --fields 'SECURITY,SSID' device wifi list)
-
-network=$(echo -e "$networks" | uniq -u | rofi -dmenu -i -selected-row 1 -p "WiFi")
